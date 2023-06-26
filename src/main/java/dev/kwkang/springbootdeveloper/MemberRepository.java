b28ed640237008b56ef64653151b7c07996e6bed
Theo <kkw.theo@gmail.com>
2023-06-26 16:28:51 +0900

    init

---
+++ src/main/java/dev/kwkang/springbootdeveloper/MemberRepository.java
@@ -0,0 +1,7 @@
+package dev.kwkang.springbootdeveloper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
